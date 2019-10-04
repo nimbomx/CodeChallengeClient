af0adebd4d5e1004ac4e5898e99894b785c57864
Ernesto Hernández <ernesto@nimbo.mx>
2019-10-04 10:33:33 -0500

    Preview Grid

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 ## STEPS
 - [x] Install fresh Nuxt
 - [x] Test axios request
+- [x] create game
+- [x] load games
+- [ ] preview grid
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,23 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        testCodeChallengeClient
-      </h1>
-      <h2 class="subtitle">
-        My super-duper Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div>
+        <button @click="loadGame(game)" v-for="game in games" :key="game.id">Game:  {{ game.id }} </button @click="loadGame(game)">
       </div>
+<hr>
+<button @click="createGame">Create Game</button>
+<button @click="createLoadGame">Create N Load Game</button>
+<pre>{{game}}</pre>
+
+<div class="d-flex">
+  some
+  <div v-for="(row,key) in grid" :key="key">
+    <div class="cell" v-for="cell in row" :key="cell.id">
+      {{cell.x}},{{cell.y}},<b>{{cell.mine}}</b>
+    </div>
+  </div>
+</div>
+
     </div>
   </div>
 </template>
@@ -35,16 +29,66 @@ export default {
   components: {
     Logo
   },
+  data(){
+    return {
+      games:[],
+      game:null,
+      grid:[]
+    }
+  },
   mounted(){
-    this.$axios.get('http://127.0.0.1:8000/api/test')
+   /* this.$axios.get('http://127.0.0.1:8000/api/test')
     this.$axios.post('http://127.0.0.1:8000/api/test')
     this.$axios.put('http://127.0.0.1:8000/api/test')
-    this.$axios.delete('http://127.0.0.1:8000/api/test')
+    this.$axios.delete('http://127.0.0.1:8000/api/test')*/
+    this.loadGames();
+  },
+  methods:{
+    loadGames(){
+      this.$axios.get('http://127.0.0.1:8000/api/games').then( ({data}) => {
+      this.games = data;
+    })
+    },
+    loadGame(game){
+      this.$axios.get('http://127.0.0.1:8000/api/game/'+game.id).then( ({data}) => {
+      this.game = data.game;
+        this.grid = data.grid;
+    })
+    },
+    createGame(){
+      this.$axios.post('http://127.0.0.1:8000/api/game/create',{
+        mines:5,
+        cols:10,
+        rows:10
+      }).then( ({data}) => {
+        this.loadGames();
+       this.game = data
+      })
+    },
+    createLoadGame(){
+      this.$axios.post('http://127.0.0.1:8000/api/game/create-n-load',{
+        mines:5,
+        cols:10,
+        rows:10
+      }).then( ({data}) => {
+        this.loadGames();
+        this.game = data.game;
+        this.grid = data.grid;
+      })
+    }
   }
 }
 </script>
 
 <style>
+.cell{
+  border: 1px solid #000;
+  width: 50px;
+  height: 50px;
+}
+.d-flex{
+  display: flex;
+}
 .container {
   margin: 0 auto;
   min-height: 100vh;
